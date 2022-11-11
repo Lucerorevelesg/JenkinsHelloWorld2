751123631f9cdb85417453af5cc35f0583fd92ab
Lucerorevelesg <117958810+Lucerorevelesg@users.noreply.github.com>
2022-11-10 20:42:51 -0600

    Update Hi.java

--- Hi.java
+++ Hi.java
@@ -1,7 +1,7 @@
 public class Hi {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello world & welcome to Jenkins training");
+		System.out.println("Made some changes");
 	}
 
 }
